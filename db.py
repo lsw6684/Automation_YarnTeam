8e58ff10c4163660e5b121647a0e5f9c999b4821
lsw6684 <lsw6684@hanmail.net>
2022-03-18 11:05:13 +0900

    파일 분리

--- db.py
+++ db.py
@@ -1,30 +1,23 @@
 import pymssql
+import secret
 
-def input_data():
+def input_data(sql):
     # DB 서버 주소
-    server = '192.168.1.9'
+    server = secret.server_address
 
     # DB 이름
-    database = 'business'
+    database = secret.db_name
 
     # 접속 ID, PW
-    username = 'sadev'
-    password = 'sadev1003'
+    username = secret.username
+    password = secret.password
 
     # MSSQL 접속
     connected_db =  pymssql.connect(server , username, password, database, charset='EUC-KR') # connected_db.autocommit(True)
     cursor = connected_db.cursor()
 
     # 명령
-    cursor.execute("""
-    SELECT  TOP 1 * 
-    FROM    dbo.acccost 
-    WHERE   dstyleno = '#7726'
-    AND     step_1 = 1 
-    AND     step_2 = 1 
-    AND     step_3 = 1
-    ;
-    """ )
+    cursor.execute(sql)
     # cursor.execute('SELECT TOP 2 * FROM dbo.acccost WHERE step_1 = 1 AND step_2 = 1 AND step_3 = 1;')
     row = cursor.fetchone()
 
--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import pandas as pd
 from test_csv import extract_data
 from db import input_data
 
-print(input_data())
+sql = """
+SELECT  TOP 1 * 
+FROM    dbo.acccost 
+WHERE   dstyleno = '#7726'
+AND     step_1 = 1 
+AND     step_2 = 1 
+AND     step_3 = 1
+;
+"""
+print(input_data(sql))
 
 print(extract_data())
