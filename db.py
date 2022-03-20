e80ef450a76accb4598079108dd7cdde5c04c59d
lsw6684 <lsw6684@hanmail.net>
2022-03-20 13:15:49 +0900

    autocommit = true

--- db.py
+++ db.py
@@ -13,16 +13,12 @@ def input_data(sql):
     password = secret.password
 
     # MSSQL 접속
-    connected_db =  pymssql.connect(server , username, password, database, charset='EUC-KR') # connected_db.autocommit(True)
+    connected_db =  pymssql.connect(server , username, password, database, charset='EUC-KR') # 
+    connected_db.autocommit(True) # 자동 커밋
     cursor = connected_db.cursor()
 
     # 명령
     cursor.execute(sql)
-    # cursor.execute('SELECT TOP 2 * FROM dbo.acccost WHERE step_1 = 1 AND step_2 = 1 AND step_3 = 1;')
-    row = cursor.fetchone()
-
-    #print(row)
-
+    
     # 연결 종료
     connected_db.close()
-    return row
--- main.py
+++ main.py
@@ -2,15 +2,9 @@ import pandas as pd
 from test_csv import extract_data
 from db import input_data
 
-sql = """
-SELECT  TOP 1 * 
-FROM    dbo.acccost 
-WHERE   dstyleno = '#7726'
-AND     step_1 = 1 
-AND     step_2 = 1 
-AND     step_3 = 1
+sql = """   
+INSERT INTO     dbo.ACC_BOARD_NO
+VALUES          ('TEST', 'DATE');
 ;
 """
-print(input_data(sql))
 
-print(extract_data())
