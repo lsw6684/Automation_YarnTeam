9ef2706900ac5394fcb599e2986123db4beb5bdf
lsw6684 <lsw6684@hanmail.net>
2022-03-21 11:05:00 +0900

    change_to_sql

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import pandas as pd
 from test_csv import extract_data
 from db import input_data
 
-sql = """   
-INSERT INTO     dbo.ACC_BOARD_NO
-VALUES          ('TEST', 'DATE');
-;
-"""
+name = 'claim_sort'
+column = ['1', '2', '3', '3', '3', '3']
+
+## 엑셀 추출 데이터를 리스트로 만들고 change_to_sql >> input_data로 넘겨주면 끝
+
+
+def change_to_sql(table_name, column_data):
+    sql =  'INSERT INTO ' + table_name + 'VALUES ('
+    for i in column_data:
+        sql += "'" + i + "',"
+    sql = sql[:-1] + ')'
+    return sql
+
+test = change_to_sql(name, column)
+# input_data(test) 
+# input_data(s)
+# if(str(type(b)) == "<class 'int'>"):
+
+
 
