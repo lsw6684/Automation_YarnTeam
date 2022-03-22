16244b96bf557787c870021952fc62af3b609310
lsw6684 <lsw6684@hanmail.net>
2022-03-22 10:44:23 +0900

    Done

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from csv import extract_data
+from datetime import datetime
+from csv_extraction import extract_data
 from db import input_data
 
 name = 'claim_sort'
 column = ['1', '2', '3', '3', '3', '3']
+excel_title = 'CHINA_COTTON'
 
-## 엑셀 추출 데이터를 리스트로 만들고 change_to_sql >> input_data로 넘겨주면 끝
+# 엑셀 추출 데이터를 리스트로 만들고 
+# input_data로 (테이블명, 데이터 리스트) 삽입. 
+# DB insert 완료.
 
 
-def change_to_sql(table_name, column_data):
-    sql =  'INSERT INTO ' + table_name + 'VALUES ('
-    for i in column_data:
-        sql += "'" + i + "',"
-    sql = sql[:-1] + ')'
-    return sql
+print(datetime.today().day)
+print(datetime.today().strftime('%b'))
 
-test = change_to_sql(name, column)
-# input_data(test) 
-# input_data(s)
-# if(str(type(b)) == "<class 'int'>"):
 
+# print(datetime.now())
 
 
